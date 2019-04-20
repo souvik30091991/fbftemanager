69e7862bfbc7f3df48bef9935dfda00955f77bf9
Das <soumadas@deloitte.com>
2019-04-20 16:53:16 +0530

    FBFTE_Soumav : Changed @Autowire class to interface from impl for FTERecorderController.java

--- fterecorderapp/src/main/java/com/fterecorder/app/controller/FTERecorderController.java
+++ fterecorderapp/src/main/java/com/fterecorder/app/controller/FTERecorderController.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fterecorder.app.model.FTERecord;
-import com.fterecorder.app.service.FTERecorderServiceImpl;
+import com.fterecorder.app.service.FTERecorderService;
 
 @RestController
 @RequestMapping(path="/fterecorder")
 public class FTERecorderController {
 
 	@Autowired
-	FTERecorderServiceImpl service;
+	FTERecorderService service;
 
 	@RequestMapping(path = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public boolean insertFTERecord(@RequestBody FTERecord[] records) {
