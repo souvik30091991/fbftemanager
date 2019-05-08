f4a671995d657883a6a53ebbcac23efe9d522dab
soumav <soumava91@gmail.com>
2019-05-09 04:05:43 +0530

    DFTE_Soumav : Changes for new fte tracker validations

--- ui-fbftemanager/src/app/fte/fte-cmp/fte-cmp.component.ts
+++ ui-fbftemanager/src/app/fte/fte-cmp/fte-cmp.component.ts
@@ -3,6 +3,8 @@ import { FteRecordClientModel } from 'src/app/client-models/fteclientmodels/fter
 import { FterecorderclientsvcService } from 'src/app/client-services/fterecorderservices/fterecorderclientsvc.service';
 import { MatDialog } from '@angular/material';
 import { OkdialogcmpComponent } from 'src/app/dialogs/okdialogcmp/okdialogcmp.component';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-fte-cmp',
@@ -13,11 +15,53 @@ export class FteCmpComponent implements OnInit {
 
     fterecordmodel: FteRecordClientModel = new FteRecordClientModel();
     fterecordmodelArr: FteRecordClientModel[] = [this.fterecordmodel];
+    postUrl = 'http://localhost:8090/ftemodifications/fterecorder/';
+    getFteCountUrl = 'http://localhost:2311/fetchftedata/getTotalFTECountByTrack/';
+    fteForm: FormGroup;
+    submitted = false;
 
-    constructor(private fterecsvc: FterecorderclientsvcService, private okDialogue: MatDialog) { }
+    constructor(private fterecsvc: FterecorderclientsvcService, private okDialogue: MatDialog,
+        private confirmDialog: MatDialog, private client: HttpClient, private formBuilder: FormBuilder) { }
 
     ngOnInit() {
+        this.fterecordmodelArr[0].ftesPerRosterCount = 0;
+        this.fteForm = this.formBuilder.group({
+            track: new FormControl({ value: this.fterecordmodelArr[0].track },
+                Validators.compose([Validators.required])),
+            weekStDt: new FormControl({ value: this.fterecordmodelArr[0].weekStDt },
+                Validators.compose([Validators.required])),
+            weekEdDt: new FormControl({ value: this.fterecordmodelArr[0].weekEdDt },
+                Validators.compose([Validators.required])),
+            ftesPerRosterCount: new FormControl({ value: this.fterecordmodelArr[0].ftesPerRosterCount, disabled: true },
+                Validators.compose([Validators.required])),
+            defectCount: new FormControl({ value: this.fterecordmodelArr[0].defectCount }, Validators.compose([Validators.required])),
+            workableDefectsCount: new FormControl({ value: this.fterecordmodelArr[0].workableDefectsCount },
+                Validators.compose([Validators.required])),
+            defectFteCount: new FormControl({ value: this.fterecordmodelArr[0].defectFteCount }, Validators.compose([Validators.required])),
+            widgetCount: new FormControl({ value: this.fterecordmodelArr[0].widgetCount }, Validators.compose([Validators.required])),
+            ftesLoanedCount: new FormControl({ value: this.fterecordmodelArr[0].ftesLoanedCount },
+                Validators.compose([Validators.required])),
+            ptoCount: new FormControl({ value: this.fterecordmodelArr[0].ptoCount }, Validators.compose([Validators.required])),
+            ftesBorrowedCount: new FormControl({ value: this.fterecordmodelArr[0].ftesBorrowedCount },
+                Validators.compose([Validators.required])),
+            ftesForPerformanceCount: new FormControl({ value: this.fterecordmodelArr[0].ftesForPerformanceCount },
+                Validators.compose([Validators.required])),
+            ftesForExtendedScenarioExecCount: new FormControl({ value: this.fterecordmodelArr[0].ftesForExtendedScenarioExecCount },
+                Validators.compose([Validators.required])),
+            medsDefAnalysisCount: new FormControl({ value: this.fterecordmodelArr[0].medsDefAnalysisCount },
+                Validators.compose([Validators.required])),
+            ndDefectAnalysis: new FormControl({ value: this.fterecordmodelArr[0].ndDefectAnalysis },
+                Validators.compose([Validators.required])),
+            excessFteCount: new FormControl({ value: this.fterecordmodelArr[0].excessFteCount }, Validators.compose([Validators.required])),
+            note: new FormControl({ value: this.fterecordmodelArr[0].note })
+            // lastName: ['', Validators.required],
+            // email: ['', [Validators.required, Validators.email]],
+            // password: ['', [Validators.required, Validators.minLength(6)]]
+        });
     }
+
+    // convenience getter for easy access to form fields
+    public get ff() { return this.fteForm.controls; }
 
     fetchFteCount() {
         this.fterecsvc.getFteCount(this.fterecordmodelArr[0].track)
@@ -28,8 +72,42 @@ export class FteCmpComponent implements OnInit {
             );
     }
     submitMe() {
-        console.log(this.fterecordmodelArr);
-        this.fterecsvc.recordIt(this.fterecordmodelArr)
+        this.submitted = true;
+
+        //finding invalid controls (don't delete! can be used in other screens for checking!):
+        /* const invalid = [];
+        const controls = this.fteForm.controls;
+        for (const name in controls) {
+            if (controls[name].invalid) {
+                invalid.push(name);
+            }
+        }
+        console.log("Invalid Controls: ---->" + invalid); */
+
+        // stop here if form is invalid
+        if (this.fteForm.invalid) {
+            return;
+        }
+        let sum: number = 0;
+        this.fterecordmodelArr.forEach((entry) => {
+            sum += Number(entry.defectCount) +
+                Number(entry.workableDefectsCount) +
+                Number(entry.defectFteCount) +
+                Number(entry.widgetCount) +
+                Number(entry.ftesLoanedCount) +
+                Number(entry.ptoCount) +
+                Number(entry.ftesBorrowedCount) +
+                Number(entry.ftesForPerformanceCount) +
+                Number(entry.ftesForExtendedScenarioExecCount) +
+                Number(entry.medsDefAnalysisCount) +
+                Number(entry.ndDefectAnalysis) +
+                Number(entry.excessFteCount);
+        });
+        if (sum !== this.fterecordmodelArr[0].ftesPerRosterCount) {
+            console.log("Fetched count : " + this.fterecordmodelArr[0].ftesPerRosterCount);
+            console.log("Calculated count : " + sum);
+        }
+        this.client.post<any>(this.postUrl, this.fterecordmodelArr)
             .subscribe(
                 data => {
                     console.log('Success!', data);
