65a27203cf7c3a4c45410b449aa118c8e40f9d71
FTE MANAGER UTILITY <laie_FTE_Manager@USMUMMUKEPAT-R.us.deloitte.com>
2019-05-14 22:56:14 +0530

    FTEManager_SOuvik : Dropdown fix

--- defectLoader/src/main/java/com/deloitte/defectLoader/DefectLoaderApplication.java
+++ defectLoader/src/main/java/com/deloitte/defectLoader/DefectLoaderApplication.java
@@ -3,9 +3,11 @@ package com.deloitte.defectLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class DefectLoaderApplication {
 
 	public static void main(String[] args) {
